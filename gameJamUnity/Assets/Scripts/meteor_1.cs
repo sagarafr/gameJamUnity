61e05d2fc055589dd1fef809b92c90762c726822
gregoire <gregoire.loens@epitech.eu>
2016-06-11 20:37:47 +0200

    Add files via upload
    j'ai gait du caca

---
+++ gameJamUnity/Assets/Scripts/meteor_1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meteor_1 : MonoBehaviour
+{
+    public Vector3 velocity = new Vector3(0.3F, 0, 0);
+    public string name = "meteor_1";
+
+	void Start ()
+    {
+        GameObject.Find(name).GetComponent<Rigidbody2D>().velocity = velocity;
+	}
+
+	void Update ()
+    {
+        GameObject.Find(name).GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
