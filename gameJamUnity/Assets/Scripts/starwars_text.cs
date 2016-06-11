30fb025ea0b6660bb9bb2316bb2aad787a9194c4
IceBlackSanctum <sylvain.corsini@gmail.com>
2016-06-11 23:21:59 +0200

    Add script for transition

---
+++ gameJamUnity/Assets/Scripts/starwars_text.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class starwars_text : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 vec = transform.position;
+		vec.z = vec.z + speed;
+		transform.position = vec;
+	}
+}
