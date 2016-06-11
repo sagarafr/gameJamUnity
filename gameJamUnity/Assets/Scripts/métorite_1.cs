6fedc0dde8674f0fc640e6fae98ef7eb0d4f47bf
gregoire <gregoire.loens@epitech.eu>
2016-06-11 20:32:16 +0200

    Add files via upload
    ajout du script de météores

---
+++ gameJamUnity/Assets/Scripts/métorite_1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class métorite_1 : MonoBehaviour
+{
+    private GameObject meteorite_1_gameobject;
+
+    // Use this for initialization
+    void Start()
+    {
+        meteorite_1_gameobject = GameObject.Find("météor_1");
+        Debug.Log(meteorite_1_gameobject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {     
+        if (meteorite_1_gameobject.transform.position.x >= -13)
+        {
+            meteorite_1_gameobject.transform.position = new Vector3(meteorite_1_gameobject.transform.position.x - 1, meteorite_1_gameobject.transform.position.y, meteorite_1_gameobject.transform.position.z);
+            meteorite_1_gameobject.transform.rotation = Quaternion.Euler(transform.rotation.x, 90, 90);
+        }
+	}
+}
