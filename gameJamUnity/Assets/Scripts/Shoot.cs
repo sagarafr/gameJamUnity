1ef00b267fccd65aaa4d7de83e5b64abce4efefd
vivaldi91 <thoveron.jeremy@neuf.fr>
2016-06-11 23:44:07 +0200

    Add files via upload
    Script move and shot

---
+++ gameJamUnity/Assets/Scripts/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject origine;
+    private int force = 500;
+    private int time = 0;
+    private List<GameObject> missiles = new List<GameObject>();
+
+    void Start()
+    {
+        if (projectile == null)
+            Debug.Log("Is NULL");
+    }
+
+    void Update() {
+        time += 1;
+        if (Input.GetButtonDown("Fire1") && time >= 50)
+        {
+            time = 0;
+            missiles.Add(( GameObject ) Instantiate(projectile, origine.transform.position, new Quaternion()));
+
+            for (int i = 0; i < missiles.Count; i++)
+            {
+                missiles[i].GetComponent<Rigidbody2D>().AddForce(new Vector3(1, 0, 0) * force, ForceMode2D.Force);
+                if (missiles[i].transform.position.x > 15)
+                {
+                    Destroy(missiles[i], 1);
+                    missiles.RemoveAt(i);
+                }
+            }
+        }
+    } 
+}
---
+++ gameJamUnity/Assets/Scripts/move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public GameObject pos;
+
+    void Start () {
+	}
+
+    void Update () {
+        if ((Input.GetKey("up") || (Input.GetKey("z"))) && pos.transform.position.y < 4.8)
+        {
+            transform.Translate(new Vector3(0, (float)0.1, 0));
+        }
+        if ((Input.GetKey("left") || (Input.GetKey("q"))) && pos.transform.position.y > -15)
+        {
+            transform.Translate(new Vector3((float)-0.1, 0, 0));
+        }
+        if ((Input.GetKey("right") || (Input.GetKey("d"))) && pos.transform.position.y < 15)
+        {
+            transform.Translate(new Vector3((float)0.1, 0, 0));
+        }
+        if ((Input.GetKey("down") || (Input.GetKey("s"))) && pos.transform.position.y > -4.8)
+        {
+            transform.Translate(new Vector3(0, (float)-0.1, 0));
+        }
+    }
+}
