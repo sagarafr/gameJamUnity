802d44976daaa49d60a8d1dc523736dda8869765
gregoire <gregoire.loens@epitech.eu>
2016-06-11 21:01:33 +0200

    Add files via upload
    ajout du script pour spawn les météores

---
+++ gameJamUnity/Assets/Scripts/spawn_meteor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn_meteor : MonoBehaviour
+{
+    public GameObject[] asteroid;
+    private int counter;
+    private int countertab;
+    private int type;
+
+	void Start ()
+    {
+        countertab = 0;
+        counter = Random.Range(1, 5);
+        while (counter > 0)
+        {
+            type = Random.Range(1, 3);
+            if (type == 1)
+            {
+                asteroid[countertab] = GameObject.Find("meteor_1");
+                asteroid[countertab].transform.position = new Vector3(Random.Range(13, 14), Random.Range(-15, 15), 0);
+            }
+            if (type == 2)
+            {
+                asteroid[countertab] = GameObject.Find("meteor_2");
+                asteroid[countertab].transform.position = new Vector3(Random.Range(0, -15), Random.Range(-15, -16), 0);
+            }
+            if (type == 3)
+            {
+               asteroid[countertab] = GameObject.Find("meteor_3");
+               asteroid[countertab].transform.position = new Vector3(Random.Range(0, -15), Random.Range(15, 16), 0);
+            }
+            counter--;
+            countertab++;
+        }
+	}
+	
+}
