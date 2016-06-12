e41c6550757147545f83368f677af9a04c664774
sagarafr <sagarafr@gmail.com>
2016-06-12 16:20:41 +0200

    Add life text

---
+++ gameJamUnity/Assets/Scripts/life_player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class life_player : MonoBehaviour
+{
+    public static life_player instance;
+    public int nb_life = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance.gameObject);
+        }
+        else
+            if (this != instance)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void remove_life()
+    {
+        nb_life -= 1;
+    }
+
+    public int get_life()
+    {
+        return nb_life;
+    }
+}
---
+++ gameJamUnity/Assets/Scripts/update_life.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class update_life : MonoBehaviour
+{
+    private Text life_text;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject life_text_gameobject = GameObject.Find("life");
+
+        if (life_text_gameobject != null)
+        {
+            life_text = life_text_gameobject.GetComponent<Text>();
+            string tmp_life = "Life : " + life_player.instance.get_life().ToString();
+            life_text.text = tmp_life;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        string tmp_life = "Life : " + life_player.instance.get_life().ToString();
+        life_text.text = tmp_life;
+/*        if (life_player.instance.get_life() <= 0)
+            SceneManager.LoadScene("");*/
+	}
+}
