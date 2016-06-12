05d61a3b321b9d0d63d1490914f888b511c7c66a
vivaldi91 <thoveron.jeremy@neuf.fr>
2016-06-12 11:59:10 +0200

    Add files via upload
    Update shooting

--- gameJamUnity/Assets/Scripts/Shoot.cs
+++ gameJamUnity/Assets/Scripts/Shoot.cs
@@ -6,51 +6,42 @@ public class Shoot : MonoBehaviour {
 
     public GameObject projectile;
     public GameObject origine;
-    private int force = 500;
+    private int force = 750;
     private int time = 0;
     private List<GameObject> missiles = new List<GameObject>();
 
     void Start()
     {
-        if (projectile == null)
-            Debug.Log("Is NULL");
     }
 
     void Update() {
         time += 1;
-        if (Input.GetButtonDown("Fire1") && time >= 50)
+        if ((Input.GetKey("space") || Input.GetButtonDown("Fire1")) && time >= 50)
         {
             AudioSource audio_to_play;
             audio_to_play = GameObject.Find("shoot_sound").GetComponent<AudioSource>();
             audio_to_play.Play();
-
             time = 0;
-            missiles.Add(( GameObject ) Instantiate(projectile, origine.transform.position, new Quaternion()));
-
+            missiles.Add((GameObject) Instantiate(projectile, origine.transform.position, new Quaternion()));
             for (int i = 0; i < missiles.Count; i++)
             {
                 GameObject tmp_gameobject = missiles[i];
-
                 if (tmp_gameobject == null)
                 {
+                    Destroy(missiles[i], 1);
                     missiles.RemoveAt(i);
                     continue;
                 }
-
                 Rigidbody2D tmp = tmp_gameobject.GetComponent<Rigidbody2D>();
-
                 if (tmp == null)
+                {
+                    Destroy(missiles[i], 1);
                     missiles.RemoveAt(i);
+                }
                 else
                 {
-                    tmp.AddForce(new Vector3(1, 0, 0) * force, ForceMode2D.Force);
-                    if (missiles[i].transform.position.x > 15)
-                    {
-                        Destroy(missiles[i], 1);
-                        missiles.RemoveAt(i);
-                    }
+                                   }
                 }
-            }
         }
     }
 }
