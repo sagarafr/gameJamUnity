b9d693ab7af63040b5d331133edf53594b4d2009
Sylvain Corsini <sylvain.corsini@gmail.com>
2016-06-12 14:29:02 +0200

    ornithorynque

---
+++ gameJamUnity/Assets/Scripts/ornithorynque.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ornithorynque : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * 0.8f);
+	}
+}
