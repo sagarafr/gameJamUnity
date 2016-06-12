879f3721a970bf0511a268d6819f791e7036eec5
sagarafr <sagarafr@gmail.com>
2016-06-12 16:03:08 +0200

    Screen changement

--- gameJamUnity/Assets/Scripts/update_score.cs
+++ gameJamUnity/Assets/Scripts/update_score.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class update_score : MonoBehaviour
 {
     private Text score_text;
+
+    public int next_level = 250;
 	// Use this for initialization
 	void Start ()
     {
@@ -23,5 +26,7 @@ public class update_score : MonoBehaviour
     {
         string tmp_score = "Score : " + score_player.instance.get_score().ToString();
         score_text.text = tmp_score;
+        if (score_player.instance.get_score() > next_level)
+            SceneManager.LoadScene("transition_game_two");
     }
 }
